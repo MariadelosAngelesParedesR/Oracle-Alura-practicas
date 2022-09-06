7364079dc15ead9f8f01e10bdc2dc2219bd4e20b
MaradelaAngelesParedesR <mparedesramirez13@hotmail.com>
2022-09-06 16:27:39 -0300

    "segunda pruebas  mp"

---
+++ javascrip/app.js
@@ -0,0 +1,21 @@
+const fact1 = 4650
+const fact2 = 4150
+
+suma = fact1 + fact2
+
+const pais = "Venezuela";
+
+function sumar(fact1,fact2){
+    fact1 + fact2
+}
+
+var semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+
+var edad = 18 
+
+if(edad >= 18){
+    alert = "Eres mayor de edad"
+}else{
+    alert = "Eres MENOR de Edad"
+}
+
